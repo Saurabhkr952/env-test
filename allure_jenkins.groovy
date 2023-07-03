823b8c3a4bfb182ad2780d18a48bcf67728b88ea
Saurabh Kumar <32189783+Saurabhkr952@users.noreply.github.com>
2023-07-03 11:49:07 +0530

    Update allure_jenkins.groovy

--- allure_jenkins.groovy
+++ allure_jenkins.groovy
@@ -1,32 +1,20 @@
 pipeline {
-    agent {
-        // volume and container name needs to modification
-    }
-    
-    stages {
-        stage('Build') {
-            steps {
-                // Build and start the containers using docker-compose
-                sh 'docker-compose up --exit-code-from container1 --exit-code-from container2'
-            }
-        }
-        
-        stage('Generate Allure Report') {
-            steps {
-                // Generate the HTML report using the Allure command-line tool inside a container
-                sh 'docker-compose run allure generate /app/allure-results -o /app/allure-report'
-            }
-        }
-        
-        stage('Publish Allure Report') {
-            steps {
-                // Copy the Allure report from the containers to the Jenkins workspace
-                sh 'docker cp container1:/app/allure-report ./allure-report-container1'
-                sh 'docker cp container2:/app/allure-report ./allure-report-container2'
-                
-                // Archive the Allure reports as artifacts
-                archiveArtifacts artifacts: 'allure-report-container1/**/*,allure-report-container2/**/*', fingerprint: true
-            }
-        }
+  agent any
+// modification needs to be done in container name pytest1 & pytest2
+  stages {
+    stage('Build and run docker-compose') {
+      steps {
+        sh 'docker-compose build'
+        sh 'docker-compose up --exit-code-from pytest1'
+        sh 'docker-compose up --exit-code-from pytest2'
+        sh 'docker-compose down'
+      }
     }
 
+    stage('Publish allure report') {
+      steps {
+        allure includeProperties: false, jdk: '', results: [[path: 'allure-results']]
+      }
+    }
+  }
+}
